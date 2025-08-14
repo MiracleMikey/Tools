a6195c755642e8cd97ce57fc1187faba0eee7e60
MiracleMikey <mm.z@hotmail.co.uk>
2025-08-14 09:37:54 +0100

    Mike Windows

--- pinger.py
+++ pinger.py
@@ -2,7 +2,7 @@ import platform
 import os
 
 #assign ip address 
-ip = "192.168.0.1"
+ip = "192.168.0.111"
 
 # Determine current running OS
 current_os = platform.system().lower()
@@ -11,7 +11,11 @@ if current_os == "windows":
     ping_command = f"ping -n 1 -w 2 {ip} > nul"
 else:
     ping_command = f"ping -c 1 -w 2 {ip} > /dev/null 2>&1"
-    
-# Print command output to the console
+
+
 exit_code = os.system(ping_command)
-print(exit_code)
+if exit_code == 0:
+    print("The host is up")
+else:
+    print("No response")
+
