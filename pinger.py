acc7ab17ebcb49c32874c2136c708b70966153c6
MiracleMikey <mm.z@hotmail.co.uk>
2025-08-14 09:09:33 +0100

    Mike Windows

---
+++ pinger.py
@@ -0,0 +1,16 @@
+import platform
+import os
+
+#assign ip address 
+ip = "192.168.0.1"
+
+# Determine current running OS
+current_os = platform.system().lower()
+
+if current_os == "windows":
+    ping_command = f"ping -n 1 -w 2 {ip} > nul"
+else:
+    ping_command = f"ping -c 1 -w 2 {ip} > /dev/null 2>&1"
+
+exit_code = os.system(ping_command)
+print(exit_code)
